d581268dc283ee4a7897d3adc4547cfef3609217
Augusto Baltazar <augusto.jaba@gmail.com>
2024-06-09 23:04:28 -0400

    Add welcome message and eslint with format on save

--- components/welcome-msg.tsx
+++ components/welcome-msg.tsx
@@ -6,8 +6,12 @@ export default function WelcomeMsg() {
   const { user, isLoaded } = useUser()
   return (
     <div className="space-y-2 mb-4">
-      <h2 className="text-2xl lg:text-4xl text-white font-medium">Bem vindo { isLoaded? ", " : " "}{user?.firstName} 👋</h2>
-      <p className="text-sm lg:text-base text-[#89b6fd]">Este é seu relatório de visão geral</p>
+      <h2 className="text-2xl lg:text-4xl text-white font-medium">
+        Bem vindo{isLoaded ? ", " : " "}{user?.firstName} 👋
+      </h2>
+      <p className="text-sm lg:text-base text-[#89b6fd]">
+        Este é seu relatório de visão geral
+      </p>
     </div>
   )
 }
